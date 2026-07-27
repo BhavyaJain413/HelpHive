adfc3ce6e74ab8fef996f0300d7b48d6bc10c3b7
Bhavya Jain <bhavyajain3009@gmail.com>
2026-07-27 17:43:32 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,21 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+  baseURL: "https://helphive-api.onrender.com/api",
 });
 
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if(token){
-
-        config.headers.Authorization = `Bearer ${token}`;
-
-    }
-
-    return config;
-
+  return config;
 });
 
 export default api;
